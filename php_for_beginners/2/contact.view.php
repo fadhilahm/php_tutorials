2eaa9748f0c65736bd541f12454d5820e77a3e5a
fadhilahm <fadhilahmetra@gmail.com>
2024-09-13 20:21:08 +0900

    Add PHP files for about and contact pages

--- php_for_beginners/2/contact.view.php
+++ php_for_beginners/2/contact.view.php
@@ -19,9 +19,9 @@
                         </div>
                         <div class="hidden md:block">
                             <div class="ml-10 flex items-baseline space-x-4">
-                                <a href="/" class="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white">Home</a>
+                                <a href="/" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Home</a>
                                 <a href="/about.php" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">About</a>
-                                <a href="/contact.php" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Contact</a>
+                                <a href="/contact.php" class="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white">Contact</a>
                             </div>
                         </div>
                     </div>
@@ -68,13 +68,13 @@
 
         <header class="bg-white shadow">
             <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-                <h1 class="text-3xl font-bold tracking-tight text-gray-900">Home</h1>
+                <h1 class="text-3xl font-bold tracking-tight text-gray-900">Contact</h1>
             </div>
         </header>
         <main>
             <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
                 <!-- Your content -->
-                Hello Happy World!
+                 Hello Happy World!
             </div>
         </main>
     </div>
