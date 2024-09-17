edfda31fe229c6a6b1968f41ac2edd8e604ed60f
fadhilahm <fadhilahmetra@gmail.com>
2024-09-17 17:38:11 +0900

    Add view files and restructure layout for PHP Beginner tutorial

--- php_for_beginners/2/views/partials/nav.php
+++ php_for_beginners/2/views/partials/nav.php
@@ -1,16 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <script src="https://cdn.tailwindcss.com"></script>
-</head>
-
-<body class="h-full">
-    <div class="min-h-full">
-        <nav class="bg-gray-800">
+<nav class="bg-gray-800">
             <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
                 <div class="flex h-16 items-center justify-between">
                     <div class="flex items-center">
@@ -20,8 +8,8 @@
                         <div class="hidden md:block">
                             <div class="ml-10 flex items-baseline space-x-4">
                                 <a href="/" class="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white">Home</a>
-                                <a href="/about.php" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">About</a>
-                                <a href="/contact.php" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Contact</a>
+                                <a href="/about.php" class="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white">About</a>
+                                <a href="/contact.php" class="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white">Contact</a>
                             </div>
                         </div>
                     </div>
@@ -65,20 +53,3 @@
                 </div>
             </div>
         </nav>
-
-        <header class="bg-white shadow">
-            <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-                <h1 class="text-3xl font-bold tracking-tight text-gray-900">Home</h1>
-            </div>
-        </header>
-        <main>
-            <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-                <!-- Your content -->
-                Hello Happy World!
-            </div>
-        </main>
-    </div>
-
-</body>
-
-</html>
