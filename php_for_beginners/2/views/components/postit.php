249e883c76f878b658bfa75e6b64d43d4cb604a8
fadhilahm <fadhilahmetra@gmail.com>
2024-10-02 17:48:57 +0900

    Wrap note title in a link to navigate to the note details

--- php_for_beginners/2/views/components/postit.php
+++ php_for_beginners/2/views/components/postit.php
@@ -4,7 +4,9 @@
 echo "
         <div class='relative bg-yellow-200 p-6 w-72 h-72 rounded shadow-lg transform rotate-2 hover:rotate-0 transition duration-300 ease-in-out'>
             <div class='absolute top-1 left-1 w-4 h-4 bg-yellow-400 rounded-full'></div>
-            <div class='font-bold text-lg text-gray-800 mb-2'>{$item['title']}</div>
+            <a href='/note/{$item['id']}' class=''>
+              <div class='font-bold text-lg text-gray-800 mb-2'>{$item['title']}</div>
+            </a>
             <p class='text-gray-700'>
                 {$item['content']}
             </p>
