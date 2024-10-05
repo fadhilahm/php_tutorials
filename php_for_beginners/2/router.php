545a9a0754aa07196b8bc006688df77b1d53c7ad
fadhilahm <fadhilahmetra@gmail.com>
2024-10-05 11:29:10 +0900

    Refactor router.php to use separate routes file

--- php_for_beginners/2/router.php
+++ php_for_beginners/2/router.php
@@ -1,13 +1,5 @@
 <?php
 
-$routes = [
-    "/" => "controllers/index.php",
-    "/about" => "controllers/about.php",
-    "/contact" => "controllers/contact.php",
-    "/notes" => "controllers/notes.php",
-    "/note" => "controllers/note.php",
-];
-
 $routeToController = function($uri, $routes, $db): void {
     $basePath = "/" . explode(separator: "/", string: $uri)[1];
     if (array_key_exists(key: $basePath, array: $routes)) {
@@ -17,4 +9,5 @@ $routeToController = function($uri, $routes, $db): void {
     }
 };
 
+$routes = require('./routes.php');
 $routeToController(uri: parse_url(url: $_SERVER['REQUEST_URI'])['path'], routes: $routes, db: $db);
