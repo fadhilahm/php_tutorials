843272f6b7b8d222c05766a13d16fea70588cb94
fadhilahm <fadhilahmetra@gmail.com>
2024-12-20 20:51:16 +0900

    Add create new note button and update banner in notes view

--- php_for_beginners/2/views/notes.view.php
+++ php_for_beginners/2/views/notes.view.php
@@ -1,6 +1,6 @@
 <?php require('partials/head.php')  ?>
 <?php require('partials/nav.php')  ?>
-<?php require('partials/banner.php')  ?>
+<?php require('partials/banner-notes.php')  ?>
 
 <main>
     <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8 flex-row flex flex-wrap gap-x-4 gap-y-6">
---
+++ php_for_beginners/2/views/partials/banner-notes.php
@@ -0,0 +1,13 @@
+<?php
+
+echo '
+<header class="bg-white shadow flex w-full items-center justify-between px-[22rem] py-6">
+    <div>
+        <h1 class="text-3xl font-bold tracking-tight text-gray-900">
+' . $banner . '
+        </h1>
+    </div>
+    <div>
+    Placeholders for buttons
+    </div>
+</header>';
