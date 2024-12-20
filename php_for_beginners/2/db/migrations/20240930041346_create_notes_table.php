887dadc0ebc3f00b19c8dbd810cc3a10a73a692b
fadhilahm <fadhilahmetra@gmail.com>
2024-12-20 19:53:04 +0900

    Refactor database setup and seeding process for improved clarity and functionality

--- php_for_beginners/2/db/migrations/20240930041346_create_notes_table.php
+++ php_for_beginners/2/db/migrations/20240930041346_create_notes_table.php
@@ -6,23 +6,12 @@ use Phinx\Migration\AbstractMigration;
 
 final class CreateNotesTable extends AbstractMigration
 {
-    /**
-     * Change Method.
-     *
-     * Write your reversible migrations using this method.
-     *
-     * More information on writing migrations is available here:
-     * https://book.cakephp.org/phinx/0/en/migrations.html#the-change-method
-     *
-     * Remember to call "create()" or "update()" and NOT "save()" when working
-     * with the Table class.
-     */
     public function change(): void
     {
         $notes = $this->table('notes', ['id' => true]);
         $notes->addColumn('title', 'string', ['limit' => 100])
             ->addColumn('content', 'text')
-            ->addColumn('user_id', 'integer')
+            ->addColumn('user_id', 'integer', ['signed' => false])
             ->addColumn('created_at', 'datetime')
             ->addColumn('updated_at', 'datetime');
 
--- php_for_beginners/2/db/seeds/NotesSeeder.php
+++ php_for_beginners/2/db/seeds/NotesSeeder.php
@@ -6,14 +6,13 @@ use Phinx\Seed\AbstractSeed;
 
 class NotesSeeder extends AbstractSeed
 {
-    /**
-     * Run Method.
-     *
-     * Write your database seeder using this method.
-     *
-     * More information on writing seeders is available here:
-     * https://book.cakephp.org/phinx/0/en/seeding.html
-     */
+    public function getDependencies(): array
+    {
+        return [
+            'UsersSeeder',
+        ];
+    }
+
     public function run(): void
     {
         $data = [];
