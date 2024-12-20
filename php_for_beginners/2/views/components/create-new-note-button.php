080667000e8f171a5bbdbbcd39bd2a48da0a043f
fadhilahm <fadhilahmetra@gmail.com>
2024-12-20 21:02:29 +0900

    Wrap create new note button in anchor tag for navigation to note creation page

--- php_for_beginners/2/views/components/create-new-note-button.php
+++ php_for_beginners/2/views/components/create-new-note-button.php
@@ -1,9 +1,11 @@
 <?php
 echo '
-<button class="px-4 py-2 bg-amber-300 hover:bg-amber-400 text-gray-800 font-medium rounded-lg shadow-md hover:shadow-lg transition-all duration-200 flex items-center gap-2">
-    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-        <path fill-rule="evenodd" d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z" clip-rule="evenodd" />
-    </svg>
-    Create New Note
-</button>
+<a href="/notes/create">
+    <button class="px-4 py-2 bg-amber-300 hover:bg-amber-400 text-gray-800 font-medium rounded-lg shadow-md hover:shadow-lg transition-all duration-200 flex items-center gap-2">
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+            <path fill-rule="evenodd" d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z" clip-rule="evenodd" />
+        </svg>
+        Create New Note
+    </button>
+</a>
 ';
