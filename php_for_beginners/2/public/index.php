b106f0114c3e2b410d179f5409939be3a85a69f5
fadhilahm <fadhilahmetra@gmail.com>
2025-01-16 18:33:18 +0900

    Refactor autoloading mechanism and update file paths for core functions

--- php_for_beginners/2/public/index.php
+++ php_for_beginners/2/public/index.php
@@ -2,9 +2,10 @@
 
 const BASE_PATH = __DIR__ . "/../";
 
-require BASE_PATH . "functions.php";
+require BASE_PATH . "Core/functions.php";
 
-require base_path("Response.php");
-require base_path("Database.php");
+spl_autoload_register(function ($class) {
+    require base_path("Core/{$class}.php");
+});
 
 require base_path("router.php");
