87eb18cf290dd4244a1311f2f571e4f81e119601
fadhilahm <fadhilahmetra@gmail.com>
2025-01-17 16:45:18 +0900

    Refactor note creation logic in store.php; improve error handling and validation
    - Enhanced error handling by returning the view with error messages if validation fails for title and content.
    - Streamlined the note creation process by ensuring that the note is only created and inserted into the database if there are no validation errors.
    - Improved code readability and maintainability by restructuring the flow of the note creation logic.

--- php_for_beginners/2/controllers/notes/store.php
+++ php_for_beginners/2/controllers/notes/store.php
@@ -5,23 +5,28 @@ use Core\Validator;
 $errors = [];
 
 if (Validator::string($_POST["title"], 1, 100) === false) {
-        $errors["title"] = "Title must be between 1 and 100 characters";
-    }
+    $errors["title"] = "Title must be between 1 and 100 characters";
+}
 
-    if (Validator::string($_POST["content"], 1, 1000) === false) {
-        $errors["content"] = "Content must be between 1 and 1000 characters";
-    }
+if (Validator::string($_POST["content"], 1, 1000) === false) {
+    $errors["content"] = "Content must be between 1 and 1000 characters";
+}
 
-    if (count($errors) == 0) {
-        $note = [
-            "title" => $_POST["title"],
-            "content" => $_POST["content"],
-        ];
+if (count($errors) > 0) {
+    return view("notes/create", [
+        "banner" => "Create a New Note",
+        "errors" => $errors
+    ]);
+}
 
-        $notes[] = $note;
+$note = [
+    "title" => $_POST["title"],
+    "content" => $_POST["content"],
+];
 
-        $db->insert("notes", $note);
-        $newNoteId = $db->lastInsertId();
-        header("Location: /notes/".$newNoteId.  "");
-        exit();
-    }
+$notes[] = $note;
+
+$db->insert("notes", $note);
+$newNoteId = $db->lastInsertId();
+header("Location: /notes/" . $newNoteId .  "");
+exit();
